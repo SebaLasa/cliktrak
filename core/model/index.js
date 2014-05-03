90e2532e5dc26e19f3ad3b79c8c9b895e24828d8
Axel Navarro <anavarro@ironsoft.com.ar>
2014-05-02 21:22:55 -0300

    +layout page models

--- core/model/index.js
+++ core/model/index.js
@@ -4,6 +4,14 @@ var mongo = require('mongoose'),
 
 var model = module.exports;
 
+model.Company = mongo.model('Company', new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    enabled: { type: Boolean, required: true, default: true },
+    deleted: { type: Boolean, required: true, default: false },
+    timezone: { type: Number, required: true, default: 0 }
+}));
+
 model.User = mongo.model('User', new Schema({
     company: { type: ObjectId, ref: 'Company', required: true },
     email: { type: String, required: true },
@@ -12,6 +20,20 @@ model.User = mongo.model('User', new Schema({
     enabled: { type: Boolean, required: true, default: true }
 }));
 
-model.Company = mongo.model('Company', new Schema({
-    name: { type: String, required: true }
+model.Layout = mongo.model('Layout', new Schema({
+    company: { type: ObjectId, ref: 'Company', required: true },
+    editor: { type: ObjectId, ref: 'User', required: true },
+    name: { type: String, required: true },
+    deleted: { type: Boolean, required: true, default: false }
+}));
+
+model.Page = mongo.model('Page', new Schema({
+    company: { type: ObjectId, ref: 'Company', required: true },
+    layout: { type: ObjectId, ref: 'Layout', required: true },
+    internalId: { type: Number, required: true },
+    name: { type: String, required: true },
+    title: { type: String, required: true },
+    html: { type: String, required: true },
+    enabled: { type: Boolean, required: true, default: true },
+    deleted: { type: Boolean, required: true, default: false }
 }));
