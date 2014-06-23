b33b4538de0ef8563c99bb8a19d9c4be8a997e14
Axel Navarro <navarroaxel@gmail.com>
2014-06-23 16:05:57 -0300

    +barcode size control

---
+++ public/controls/barcodeSize/index.js
@@ -0,0 +1,19 @@
+app.directive("barcodeSize", function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: '/controls/barcodeSize/index.html',
+        scope: {
+            selected: '=',
+            disabled: '='
+        },
+        controller: function ($scope) {
+            $scope.sizes = [
+                { name: 'Little', value: 30 },
+                { name: 'Small', value: 50 },
+                { name: 'Medium', value: 70 },
+                { name: 'Big', value: 90 }
+            ];
+        }
+    };
+});
