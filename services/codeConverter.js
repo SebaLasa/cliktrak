9717653359a88b9908055a3d2392ebcfca31989e
Nicolas Far <nicofff@gmail.com>
2014-08-19 14:57:38 -0300

    Update barcode lib, service improvements

--- services/codeConverter.js
+++ services/codeConverter.js
@@ -1,29 +1,50 @@
 var bwipjs = require('node-bwipjs-ng');
 
-/**
- * data : String to convert to qr
- * callback: function (err,data)
- * data is a Buffer containing the image as png
- * @param data
- * @param callback
- */
-module.exports.toQR = function (data, callback) {
-    var img = bwipjs.createQR(data, function (png) {
+function generateBarcode(data, cb){
+    var img = bwipjs.createBarcode(data, function(png){
         var imgBase64 = new Buffer(png, 'binary').toString('base64');
-        callback(null, '<img src="data:image/png;base64,' + imgBase64 + '" />');
+
+        cb(null,'<img src="data:image/png;base64,' + imgBase64 + '" />');
     });
-};
+}
 
 /**
- * data : String to convert to qr
+ * text : String to convert to qr
+ * opts : scale, rotate params
  * callback (err,data)
  * data is a Buffer containing the image as png
  * @param data
  * @param callback
  */
-module.exports.toBarcode = function (data, callback) {
-    var img = bwipjs.createBarcode(data, function (png) {
-        var imgBase64 = new Buffer(png, 'binary').toString('base64');
-        callback(null, '<img src="data:image/png;base64,' + imgBase64 + '" />');
-    });
+
+module.exports.toQR = function (text, opts, callback) {
+
+    var data = {
+        text: text,
+        type : 'qrcode',
+        scale : opts.scale || 2,
+        rotate : opts.rotate || 'N'
+    }
+
+    generateBarcode(data, callback);
+
+};
+
+/**
+ * text : String to convert to qr
+ * opts : scale, rotate params
+ * callback (err,data)
+ * data is a Buffer containing the image as png
+ * @param data
+ * @param callback
+ */
+module.exports.toBarcode = function (text, opts, callback) {
+    var data = {
+        text: text,
+        type : 'interleaved2of5',
+        scale : opts.scale || 2,
+        rotate : opts.rotate || 'N'
+    }
+
+    generateBarcode(data, callback);
 };
