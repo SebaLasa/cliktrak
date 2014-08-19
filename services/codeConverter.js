d7199acce4a5bf78fe8a6f43e72b2ac930b4237e
Axel Navarro <navarroaxel@gmail.com>
2014-08-19 14:23:52 -0300

    -commented code

--- services/codeConverter.js
+++ services/codeConverter.js
@@ -7,29 +7,11 @@ var bwipjs = require('node-bwipjs-ng');
  * @param data
  * @param callback
  */
-
 module.exports.toQR = function (data, callback) {
-    /*var chunks = 0
-    var bufs = []
-    var qr = qrimage.image(data, { type: 'png' })
-
-    .on('data', function(chunk){
-            bufs[chunks++]=chunk;
-        })
-    .on('end', function () {
-        data = Buffer.concat(bufs);
-        var imgBase64 = data.toString('base64');
-        callback(null, '<img src="data:image/png;base64,' + imgBase64 + '" />');
-    })
-    .on("error",function (err){
-        callback(err,null);
-    });*/
-    var img = bwipjs.createQR(data, function(png){
+    var img = bwipjs.createQR(data, function (png) {
         var imgBase64 = new Buffer(png, 'binary').toString('base64');
-
-        callback(null,'<img src="data:image/png;base64,' + imgBase64 + '" />');
+        callback(null, '<img src="data:image/png;base64,' + imgBase64 + '" />');
     });
-
 };
 
 /**
@@ -40,9 +22,8 @@ module.exports.toQR = function (data, callback) {
  * @param callback
  */
 module.exports.toBarcode = function (data, callback) {
-    var img = bwipjs.createBarcode(data, function(png){
+    var img = bwipjs.createBarcode(data, function (png) {
         var imgBase64 = new Buffer(png, 'binary').toString('base64');
-
-        callback(null,'<img src="data:image/png;base64,' + imgBase64 + '" />');
+        callback(null, '<img src="data:image/png;base64,' + imgBase64 + '" />');
     });
 };
