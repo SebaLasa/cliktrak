5ce45ccfa7c5df37add0b33d1899404b3dc4e21d
Axel Navarro <navarroaxel@gmail.com>
2014-08-20 19:19:59 -0300

    +back link

--- public/controllers/reports/reportsController.js
+++ public/controllers/reports/reportsController.js
@@ -1,5 +1,6 @@
 angular.module('clicks').controller('reportsController', function ($scope, $http, $location, $routeParams) {
-    $http.get('/api/reports/' + $routeParams.pageType + '/' + $routeParams.id)
+    $scope.backLink = '/' + $routeParams.pageType + '/' + $routeParams.id;
+    $http.get('/api/reports' + $scope.backLink)
         .success(function (data) {
             $scope.report = data;
         });
