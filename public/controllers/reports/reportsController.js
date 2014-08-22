a4dcefe342534ebd961b159797d2e740d48498f6
Nicolas Far <nicofff@gmail.com>
2014-08-22 17:31:21 -0300

    Add charts module

--- public/controllers/reports/reportsController.js
+++ public/controllers/reports/reportsController.js
@@ -4,5 +4,25 @@ angular.module('clicks').controller('reportsController', function ($scope, $http
     $http.get('/api/reports/' + $routeParams.pageType + '/' + $routeParams.id)
         .success(function (data) {
             $scope.report = data;
+
+            $scope.daysData = {
+                "series" : ["clicks"],
+                "data" : _.map($scope.report.clicksPerDay,function(dayData){
+                    return {x:dayData["day"],y: dayData["count"]}
+                })
+            }
         });
+    var config = {
+        title: 'Tu Vieja',
+        tooltips: true,
+        labels: false,
+        legend: {
+            display: true,
+            //could be 'left, right'
+            position: 'left'
+        }
+    }
+
+    $scope.daysConfig = config;
+    $scope.daysChartType = 'bar';
 });
