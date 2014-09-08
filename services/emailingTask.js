de9f12993ef0edb173fa2f007aa434ac92e9ca67
Axel Navarro <navarroaxel@gmail.com>
2014-09-08 19:36:35 -0300

    gen messages to send

--- services/emailingTask.js
+++ services/emailingTask.js
@@ -3,6 +3,46 @@ var nodemailer = require('nodemailer'),
     model = app.model;
 
 module.exports.run = function (cb) {
+    var today = new Date();
+    model.emailing.Task.find({dateStart: { $lt: today }, dateEnd: { $gt: today }}).populate('contacts').exec(function (err, tasks) {
+        if (err) {
+            return cb(err);
+        }
+
+        async.each(tasks, function (task, messageCallback) {
+            generateMessages(task, messageCallback);
+        }, function (err) {
+            if (err) {
+                return cb(err);
+            }
+            tasks.filter(function (task) {
+                return task.messages.every(function (message) {
+                    return !!message.dateSent;
+                });
+            });
+            sendEmails(tasks, cb);
+        });
+    });
+};
+
+function generateMessages(task, callback) {
+    if (task.messages && task.messages.length) {
+        return callback();
+    }
+    if (!task.contacts || !task.contacts.length) {
+        return callback();
+    }
+
+    task.messages = task.contacts.map(function (contact) {
+        return {
+            contact: contact._id,
+            email: contact.email
+        };
+    });
+    task.save(callback);
+}
+
+function sendEmails(tasks, callback) {
     var transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
@@ -10,38 +50,25 @@ module.exports.run = function (cb) {
             pass: app.config.email.password
         }
     });
-    var today = new Date();
-    model.emailing.Task.find({dateStart: { $lt: today }, dateEnd: { $gt: today }}, function (err, tasks) {
-        if (err) {
-            return cb(err);
-        }
 
-        // TODO AN generate messages if the collection is null or empty.
-        tasks.filter(function (task) {
-            return task.messages.every(function (message) {
-                return !!message.dateSent;
+    async.each(tasks, function (task, taskCallback) {
+        async.each(task.messages, function (message, messageCallback) {
+            if (message.dateSent) {
+                return messageCallback();
+            }
+            sendEmail(transporter, task, message, function (err) {
+                message.dateSent = new Date();
+                message.error = err;
+                messageCallback(err);
+            });
+        }, function (err) {
+            task.error = err;
+            task.save(function (err) {
+                taskCallback(err);
             });
         });
-
-        async.each(tasks, function (task, taskCallback) {
-            async.each(task.messages, function (message, messageCallback) {
-                if (message.dateSent) {
-                    return messageCallback();
-                }
-                sendEmail(transporter, task, message, function (err) {
-                    message.dateSent = new Date();
-                    message.error = err;
-                    messageCallback(err);
-                });
-            }, function (err) {
-                task.error = err;
-                task.save(function (err) {
-                    taskCallback(err);
-                });
-            });
-        }, cb);
-    });
-};
+    }, callback);
+}
 
 function sendEmail(transporter, task, message, callback) {
     var mailOptions = {
