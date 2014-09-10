ea39f72380e6f3d94555a69c4bafdc422a037818
Nicolas Far <nicofff@gmail.com>
2014-09-10 15:07:38 -0300

    Crud Users

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -21,61 +21,21 @@ angular.module('admin', ['ngRoute', 'ui.tinymce', 'colorpicker.module', 'ui.date
                 controller: 'companyDeleteController',
                 templateUrl:'/views/admin/companies/delete.html'
             })
-            .when('/pages', {
-                controller: 'pagesController',
-                templateUrl: '/views/pages/index.html'
+            .when('/users', {
+                controller: 'usersController',
+                templateUrl: '/views/admin/users/index.html'
             })
-            .when('/pages/new', {
-                controller: 'pageEditorController',
-                templateUrl: '/views/pages/editor.html'
+            .when('/users/new', {
+                controller: 'userEditorController',
+                templateUrl: '/views/admin/users/editor.html'
             })
-            .when('/pages/:id', {
-                controller: 'pageEditorController',
-                templateUrl: '/views/pages/editor.html'
+            .when('/users/:id', {
+                controller: 'userEditorController',
+                templateUrl: '/views/admin/users/editor.html'
             })
-            .when('/customPages', {
-                controller: 'customPagesController',
-                templateUrl: '/views/customPages/index.html'
-            })
-            .when('/customPages/new', {
-                controller: 'customPageEditorController',
-                templateUrl: '/views/customPages/editor.html'
-            })
-            .when('/customPages/:id', {
-                controller: 'customPageEditorController',
-                templateUrl: '/views/customPages/editor.html'
-            })
-            .when('/customPages/delete/:id', {
-                controller: 'customPagesDeleteController',
-                templateUrl: '/views/customPages/delete.html'
-            })
-            .when('/reports/:pageType/:id', {
-                controller: 'reportsController',
-                templateUrl: '/views/reports/index.html'
-            })
-            .when('/campaigns', {
-                controller: 'campaignsController',
-                templateUrl: '/views/campaigns/index.html'
-            })
-            .when('/campaigns/new', {
-                controller: 'campaignEditorController',
-                templateUrl: '/views/campaigns/editor.html'
-            })
-            .when('/campaigns/:id', {
-                controller: 'campaignEditorController',
-                templateUrl: '/views/campaigns/editor.html'
-            })
-            .when('/contacts', {
-                controller: 'contactsController',
-                templateUrl: '/views/contacts/index.html'
-            })
-            .when('/contacts/new', {
-                controller: 'contactEditorController',
-                templateUrl: '/views/contacts/editor.html'
-            })
-            .when('/contacts/:id', {
-                controller: 'contactEditorController',
-                templateUrl: '/views/contacts/editor.html'
+            .when('/users/delete/:id', {
+                controller: 'userDeleteController',
+                templateUrl: '/views/admin/users/delete.html'
             })
             .otherwise({ redirectTo: '/' });
 
--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -4,7 +4,7 @@ var model = app.model,
 
 module.exports = function (router) {
 
-    router.get('/users/generateAdmin', function (req, res, next) {
+    /*router.get('/users/generateAdmin', function (req, res, next) {
         model.Company.findOne({internalId:0}, function (err, company){
             if (err) {
                 return next(Error.create('An error occurred trying get the root company.', err));
@@ -20,6 +20,65 @@ module.exports = function (router) {
                 }
                 res.send("SuperAdmin creado");
             });
+        });
+    });*/
+
+    router.get('/users', function (req, res, next) {
+        model.User.find({deleted: false}).populate("company").exec(function (err, users) {
+            if (err) {
+                return next(Error.create('An error occurred trying get the Users.', { }, err));
+            }
+            res.json(users);
+        });
+    });
+
+    router.get('/users/:id', function (req, res, next) {
+        if (!validate.objectId(req.params.id)) {
+            return res.status(400).end();
+        }
+        model.User.findById(req.params.id, function (err, user) {
+            if (err) {
+                return next(Error.create('An error occurred trying get the Company.', { }, err));
+            }
+            user.password = '';
+            res.json(user);
+        });
+    });
+
+    router.post('/users', function (req, res, next) {
+        var user = new model.User(req.body);
+        user.password = hash(user.password);
+        user.save(function (err, user) {
+            if (err) {
+                return next(Error.create('An error occurred trying save the company.', { }, err));
+            }
+            res.status(201).end();
+        });
+
+    });
+
+    router.put('/users/:id', function (req, res, next) {
+        delete req.body._id;
+        if (req.body.password == ''){
+            delete req.body.password;
+        }else {
+            req.body.password = hash(req.body.password);
+        }
+        model.User.findByIdAndUpdate(req.params.id, req.body, function (err, user) {
+            if (err) {
+                return next(Error.create('An error occurred trying update the Company.', { }, err));
+            }
+            res.status(200).end();
+        });
+    });
+
+    router.delete('/users/:id', function (req, res, next) {
+        req.body.deleted= true;
+        model.User.findByIdAndUpdate(req.params.id, req.body, function (err, user) {
+            if (err) {
+                return next(Error.create('An error occurred trying delete the Company.', { }, err));
+            }
+            res.status(200).end();
         });
     });
 
