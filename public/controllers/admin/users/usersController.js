b95f043bbea6e32f936d9a20b7ef9d01791cd35d
Nicolas Far <nicofff@gmail.com>
2014-09-10 15:07:50 -0300

    Crud Users

---
+++ public/controllers/admin/users/usersController.js
@@ -0,0 +1,12 @@
+angular.module('admin').controller('usersController', function ($scope, $http) {
+    $http.get('/api/users/')
+        .success(function (data, status) {
+            $scope.users = data;
+        });
+    $scope.deletePage = function (id) {
+        $http.delete('/api/users/' + id)
+            .success(function (data, status) {
+
+            });
+    };
+});
