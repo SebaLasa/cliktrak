7580c18c9caa825f165c9981911e09284b0c7d68
Axel Navarro <navarroaxel@gmail.com>
2014-09-12 14:11:11 -0300

    fixed save urlconfig for pages

--- routes/api/pages/index.js
+++ routes/api/pages/index.js
@@ -27,47 +27,65 @@ module.exports = function (router) {
     });
 
     router.post('/pages', function (req, res, next) {
-        var page = new model.Page(req.body);
-        page.editor = req.user._id;
-        page.company = req.company._id;
-        model.Page.find({company: req.company._id}).sort('-internalId').findOne(function (err, lastPage) {
+        var urlConfiguration = new model.UrlConfiguration(req.body.urlConfiguration);
+        urlConfiguration.save(function (err, urlConfiguration) {
             if (err) {
-                return next(Error.create('An error occurred trying get the last Page.', { }, err));
+                return next(Error.create('An error occurred trying save the URL configuration.', { }, err));
             }
-            page.internalId = lastPage ? lastPage.internalId + 1 : 1;
-            page.save(function (err, page) {
+            var page = new model.Page(req.body.page);
+            page.editor = req.user._id;
+            page.company = req.company._id;
+            page.urlConfiguration = urlConfiguration._id;
+            model.Page.find({company: req.company._id}).sort('-internalId').findOne(function (err, lastPage) {
                 if (err) {
-                    return next(Error.create('An error occurred trying save the Page.', { }, err));
+                    return next(Error.create('An error occurred trying get the last Page.', { }, err));
                 }
-                res.status(201).end();
+                page.internalId = lastPage ? lastPage.internalId + 1 : 1;
+                page.save(function (err, page) {
+                    if (err) {
+                        return next(Error.create('An error occurred trying save the Page.', { }, err));
+                    }
+                    res.status(201).end();
+                });
             });
         });
     });
 
     router.put('/pages/:id', function (req, res, next) {
-        model.Page.findByIdAndUpdate(req.params.id, req.body, function (err, page) {
+        delete req.body.page._id;
+        delete req.body.page.urlConfiguration;
+        page.editor = req.user._id;
+        page.company = req.company._id;
+        model.Page.findOneAndUpdate({_id: req.params.id, deleted: false, company: req.company._id}, req.body.page, function (err, page) {
             if (err) {
                 return next(Error.create('An error occurred trying update the Page.', { }, err));
             }
-            res.status(200).end();
+            if (!page) {
+                return res.status(404).end();
+            }
+            model.UrlConfiguration.findByIdAndUpdate(page.urlConfiguration, req.body.urlConfiguration, function (err, urlConfiguration) {
+                if (err) {
+                    return next(Error.create('An error occurred trying update the URL configuration.', { }, err));
+                }
+                res.status(200).end();
+            });
         });
     });
 
     router.delete('/pages/:id', function (req, res, next) {
-        model.Page.findOne({_id: req.params.id, deleted: false, company: req.company._id}, function (err, page) {
+        var page = {
+            editor: req.user._id,
+            company: req.company._id,
+            deleted: true
+        };
+        model.Page.findOneAndUpdate({_id: req.params.id, deleted: false, company: req.company._id}, page, function (err, page) {
             if (err) {
                 return next(Error.create('An error occurred trying delete the Page.', { }, err));
             }
             if (!page) {
                 return res.status(404).end();
             }
-            page.deleted = true;
-            page.save(function (err) {
-                if (err) {
-                    return next(Error.create('An error occurred trying delete the Page.', { }, err));
-                }
-                res.status(200).end();
-            });
+            res.status(200).end();
         });
     });
 
