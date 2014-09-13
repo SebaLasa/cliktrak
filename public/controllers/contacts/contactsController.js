8b88b4f66c1a94d9536d3f581a116f16250eb360
Pato <pboffino@hotmail.com>
2014-09-13 19:54:59 -0300

    embellecimiento de boton: con una directiva momentaneamente en el controller

--- public/controllers/contacts/contactsController.js
+++ public/controllers/contacts/contactsController.js
@@ -13,4 +13,17 @@ angular.module('clicks').controller('contactsController', function ($scope, $htt
             });
     };
     loadContacts();
-});
+}).directive("fileread", [function () {
+    return {
+        scope: {
+            fileread: "="
+        },
+        link: function (scope, element, attributes) {
+            element.bind("change", function (changeEvent) {
+                scope.$apply(function () {
+                    scope.fileread = changeEvent.target.files[0];
+                });
+            });
+        }
+    }
+}]);
