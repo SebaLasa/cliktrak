78187e6648ef9fc9f844ad43903f2c5d13f2dead
Axel Navarro <navarroaxel@gmail.com>
2014-11-27 11:50:46 -0300

    +run tasks button

--- public/controllers/admin/home/homeController.js
+++ public/controllers/admin/home/homeController.js
@@ -1,3 +1,9 @@
-angular.module('admin').controller('homeController', function ($scope) {
-
+angular.module('admin').controller('homeController', function ($scope, $http) {
+    $scope.ranSuccessfully = false;
+    $scope.run = function () {
+        $scope.ranSuccessfully = false;
+        $http.get('/api/run').success(function (data) {
+            $scope.ranSuccessfully = true;
+        });
+    };
 });
