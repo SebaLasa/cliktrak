c7ecaebacb1d44cc1409c548bcac5e145f2c0717
Axel Navarro <navarroaxel@gmail.com>
2014-11-27 13:32:06 -0300

    fixed clicks repo

--- routes/api/reports/index.js
+++ routes/api/reports/index.js
@@ -14,11 +14,11 @@ module.exports = function (router) {
         }).groupBy(function (click) {
             return click.timestamp.getDate() + '/' + (click.timestamp.getMonth() + 1);
         }).map(function (x, day) {
-            return { day: day, count: x.length };
+            return {day: day, count: x.length};
         }).value();
 
         var devices = _(clicks).groupBy('device').map(function (x, agent) {
-            return { device : agent, count: x.length};
+            return {device: agent, count: x.length};
         }).value();
         return {
             clicksCount: clicks.length,
@@ -28,63 +28,63 @@ module.exports = function (router) {
     }
 
     router.get('/reports/pages/:id', function (req, res, next) {
-        model.TrackedClick.find({ page: req.params.id }).sort('timestamp').exec(function (err, clicks) {
+        model.TrackedClick.find({page: req.params.id}).sort('timestamp').exec(function (err, clicks) {
             if (err) {
-                return next(Error.create('An error occurred trying get the page\'s report.', { id: req.params.id }, err));
+                return next(Error.create('An error occurred trying get the page\'s report.', {id: req.params.id}, err));
             }
             res.json(getReport(clicks));
         });
     });
 
     router.get('/reports/customPages/:id', function (req, res, next) {
-        model.TrackedClick.find({ customPage: req.params.id }, function (err, clicks) {
+        model.TrackedClick.find({customPage: req.params.id}, function (err, clicks) {
             if (err) {
-                return next(Error.create('An error occurred trying get the page\'s report.', { id: req.params.id }, err));
+                return next(Error.create('An error occurred trying get the page\'s report.', {id: req.params.id}, err));
             }
             res.json(getReport(clicks));
         });
     });
 
     router.get('/reports/pages/:id/download', function (req, res, next) {
-        model.TrackedClick.find({ page: req.params.id })
-        .populate(['page'])
-        .exec(function (err, clicks) {
-            if (err) {
-                return next(Error.create('An error occurred trying get the page\'s report.', { id: req.params.id }, err));
-            }
-            var header= ['IP','timestamp','device','page','reference value','agent'];
-            var dataArray = _.map(clicks,function(click){
-                return [
-                    click.ipAddress,
-                    click.timestamp,
-                    click.device,
-                    click.page.name,
-                    click.valueReference,
-                    click.agent
-                ];
-            });
-            dataArray.unshift(header);
+        model.TrackedClick.find({page: req.params.id})
+            .populate(['page'])
+            .exec(function (err, clicks) {
+                if (err) {
+                    return next(Error.create('An error occurred trying get the page\'s report.', {id: req.params.id}, err));
+                }
+                var header = ['IP', 'timestamp', 'device', 'page', 'reference value', 'agent'];
+                var dataArray = _.map(clicks, function (click) {
+                    return [
+                        click.ipAddress,
+                        click.timestamp,
+                        click.device,
+                        click.page.name,
+                        click.valueReference,
+                        click.agent
+                    ];
+                });
+                dataArray.unshift(header);
 
-            stringify(dataArray,function(err,data){
-               if (err){
-                   return next(Error.create('An error occurred trying get the page\'s report.', { id: req.params.id }, err));
-               }
-                res.set('Content-Type', 'text/csv');
-                res.setHeader('Content-disposition', 'attachment; filename=page.csv');
-                res.send(data);
+                stringify(dataArray, {delimiter: ';'}, function (err, data) {
+                    if (err) {
+                        return next(Error.create('An error occurred trying get the page\'s report.', {id: req.params.id}, err));
+                    }
+                    res.set('Content-Type', 'text/csv');
+                    res.setHeader('Content-disposition', 'attachment; filename=clicks ' + req.params.id + '.csv');
+                    res.send(data);
+                });
             });
-        });
     });
 
     router.get('/reports/customPages/:id/download', function (req, res, next) {
-        model.TrackedClick.find({ customPage: req.params.id })
+        model.TrackedClick.find({customPage: req.params.id})
             .populate(['customPage'])
             .exec(function (err, clicks) {
                 if (err) {
-                    return next(Error.create('An error occurred trying get the page\'s report.', { id: req.params.id }, err));
+                    return next(Error.create('An error occurred trying get the page\'s report.', {id: req.params.id}, err));
                 }
-                var header= ['IP','timestamp','device','customPage','reference value','agent'];
-                var dataArray = _.map(clicks,function(click){
+                var header = ['IP', 'timestamp', 'device', 'customPage', 'reference value', 'agent'];
+                var dataArray = _.map(clicks, function (click) {
                     return [
                         click.ipAddress,
                         click.timestamp,
@@ -96,12 +96,12 @@ module.exports = function (router) {
                 });
                 dataArray.unshift(header);
 
-                stringify(dataArray,function(err,data){
-                    if (err){
-                        return next(Error.create('An error occurred trying get the page\'s report.', { id: req.params.id }, err));
+                stringify(dataArray, {delimiter: ';'}, function (err, data) {
+                    if (err) {
+                        return next(Error.create('An error occurred trying get the page\'s report.', {id: req.params.id}, err));
                     }
                     res.set('Content-Type', 'text/csv');
-                    res.setHeader('Content-disposition', 'attachment; filename=page.csv');
+                    res.setHeader('Content-disposition', 'attachment; filename=clicks ' + req.params.id + '.csv');
                     res.send(data);
                 });
             });
