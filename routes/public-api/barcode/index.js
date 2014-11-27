cf6979bf2e920e1c605f557b62849c6d079cb44a
Axel Navarro <navarroaxel@gmail.com>
2014-11-27 11:41:33 -0300

    moved hardcoded strings to const

--- routes/public-api/barcode/index.js
+++ routes/public-api/barcode/index.js
@@ -1,5 +1,6 @@
 var CodeConverter = require("../../../services/codeConverter");
 
+var contentTypeGif = 'image/gif';
 module.exports = function (router) {
     router.get('/barcode/:sizing/:string', function (req, res, next) {
         var data = req.params.string;
@@ -7,25 +8,21 @@ module.exports = function (router) {
             if (err) {
                 return next(Error.create('An error occurred trying to create the barcode.', { }, err));
             }
-            // TODO unhardcode mimeType
-            res.writeHead(200, { 'Content-Type': 'image/gif' });
+            res.writeHead(200, { 'Content-Type': contentTypeGif });
             res.end(img, 'binary');
         });
     });
 
     router.get('/qr/:sizing/:string', function (req, res, next) {
-
         var data = req.params.string;
         data = new Buffer(data, 'base64').toString('ascii');
         CodeConverter.toQR(data, {scale: req.params.sizing}, function (err, img) {
             if (err) {
                 return next(Error.create('An error occurred trying to create the barcode.', { }, err));
             }
-            // TODO unhardcode mimeType
-            res.writeHead(200, { 'Content-Type': 'image/gif' });
+            res.writeHead(200, { 'Content-Type': contentTypeGif });
             res.end(img, 'binary');
         });
     });
-
     return router;
 };
